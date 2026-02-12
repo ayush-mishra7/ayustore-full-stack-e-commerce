2b10641a3e7a7ca32b731bec8bb195e51e22f344
ayush-mishra7 <ayush963ash@gmail.com>
2026-02-12 21:39:42 +0530

    chore: Add debug logging for Google Client ID

--- backend/src/main/java/com/ayustore/AyuStoreApplication.java
+++ backend/src/main/java/com/ayustore/AyuStoreApplication.java
@@ -8,6 +8,14 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class AyuStoreApplication {
 
+    @org.springframework.context.annotation.Bean
+    public org.springframework.boot.CommandLineRunner debugRunner(
+            @org.springframework.beans.factory.annotation.Value("${spring.security.oauth2.client.registration.google.client-id}") String clientId) {
+        return args -> {
+            System.out.println("DEBUG: Google Client ID is: " + clientId);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AyuStoreApplication.class, args);
     }
